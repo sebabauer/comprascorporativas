aab0aa4c270e50c8bd3ed596bf51fcafe1372103
sebastian fuenza rails <sebastian.fuenza@gmail.com>
2018-04-16 11:20:12 -0300

    los servicios se ven en el Show de Companies

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,5 @@ Rails.application.routes.draw do
       delete 'remove_keyword/:keyword_id', to: 'companies#remove_keyword', as: 'remove_keyword'
     end
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "companies#index"
 end
